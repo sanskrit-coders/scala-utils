79ec08d8d4c23bcd2a2ee7bbf962c2b6b6f9f037
vvasuki <vishvas.vasuki@gmail.com>
2019-11-10 08:28:54 +0530

    Content update.

--- src/main/scala/sanskrit_coders/RichHttpAkkaClient.scala
+++ src/main/scala/sanskrit_coders/RichHttpAkkaClient.scala
@@ -1,8 +1,10 @@
 package sanskrit_coders
 
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, StatusCodes}
+import akka.stream.scaladsl.FileIO
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.util.ByteString
 import org.slf4j.{Logger, LoggerFactory}
@@ -13,47 +15,40 @@ import scala.concurrent.{ExecutionContext, Future}
   * A client robust to redirects and such. Copied and adapted from https://github.com/akka/akka-http/issues/195
   *
   * Usage: call httpClientWithRedirect().
-  *   implicit val actorMaterializer = ActorMaterializer()
-  *   implicit val executionContext = system.dispatcher
-  *   private val simpleClient: HttpRequest => Future[HttpResponse] = Http(context.system).singleRequest(_: HttpRequest)
-  *   private val redirectingClient: HttpRequest => Future[HttpResponse] = RichHttpClient.httpClientWithRedirect(simpleClient)
-  *   RichHttpClient.httpResponseToString(redirectingClient(HttpRequest(uri = uri))).map(responseString => {
-      *log.debug(responseString)
-      *val archiveItem = jsonHelper.fromString[ItemInfo](responseString)
-      *archiveItem.toPodcast(filePattern = podcastRequest.filePattern, useArchiveOrder = podcastRequest.useArchiveOrder, podcast = podcastRequest.podcastTemplate)
-    *})
- **
- *
- To dump to file:
-      *val httpResponseFuture = redirectingClient(HttpRequest(uri = dict.dictTarUrl))
- **
- val destinationPath = Paths.get(dict.destinationFolder,  dict.dictTarUrl.split("/").last)
-      *assert(new java.io.File(dict.destinationFolder).mkdirs())
-      *val fileSink = FileIO.toPath(destinationPath)
-      *val ioResultFuture = httpResponseFuture.flatMap(response => {
-        *response.entity.dataBytes.runWith(fileSink)
-      *})
+  *   (Optional:) implicit val actorSystem = ActorSystem("xyz")
+  *   private val redirectingClient: HttpRequest => Future[HttpResponse] = getClientWithAkkaSystem()
+  *   RichHttpClient.httpResponseToString(redirectingClient(HttpRequest(uri = uri))).map(responseString => {log.debug(responseString)})
  *
  */
 //noinspection ScalaDocMissingParameterDescription
-object RichHttpClient {
+object RichHttpAkkaClient {
   private val log: Logger = LoggerFactory.getLogger(this.getClass)
   type HttpClient = HttpRequest ⇒ Future[HttpResponse]
+
+  def getClientWithAkkaSystem()(implicit system: ActorSystem = ActorSystem("httpAkka")): HttpRequest => Future[HttpResponse] = {
+    import scala.concurrent._
+    implicit val ec: ExecutionContext = system.dispatcher
+    implicit val actorMaterializer = ActorMaterializer()
+    val simpleClient: HttpRequest => Future[HttpResponse] = Http().singleRequest(_: HttpRequest)
+    val redirectingClient: HttpRequest => Future[HttpResponse] = RichHttpAkkaClient.httpClientWithRedirect(simpleClient)
+    redirectingClient
+  }
 
   /**
     * Given a response, follow a redirect - or just yield the response.
     *
     * @param client A function of the type HttpClient
     * @param response
-    * @param materializer
+    * @param system
     * @return
     */
   // We'll use this function below in httpClientWithRedirect.
-  def redirectOrResult(client: HttpClient)(response: HttpResponse)(implicit materializer: Materializer): Future[HttpResponse] =
+  def redirectOrResult(client: HttpClient)(response: HttpResponse)(implicit system: ActorSystem = ActorSystem("httpAkka")): Future[HttpResponse] =
     response.status match {
       case StatusCodes.Found | StatusCodes.MovedPermanently | StatusCodes.SeeOther ⇒
         val newUri = response.header[Location].get.uri
         // Always make sure you consume the response entity streams (of type Source[ByteString,Unit]) by for example connecting it to a Sink (for example response.discardEntityBytes() if you don’t care about the response entity), since otherwise Akka HTTP (and the underlying Streams infrastructure) will understand the lack of entity consumption as a back-pressure signal and stop reading from the underlying TCP connection!
+        implicit val materializer = ActorMaterializer()
         response.discardEntityBytes()
         log.info(s"redirected to ${newUri.toString()}")
 
@@ -72,12 +67,12 @@ object RichHttpClient {
     * Turns a  HttpClient into a Http client which handles redirects.
     *
     * @param client
-    * @param ec
-    * @param materializer
+    * @param system
     * @return a recursive function of the type HttpClient
     */
-  def httpClientWithRedirect(client: HttpClient)(implicit ec: ExecutionContext, materializer: Materializer): HttpClient = {
+  def httpClientWithRedirect(client: HttpClient)(implicit system: ActorSystem = ActorSystem("httpAkka")): HttpClient = {
     // We are defining a function below!
+    implicit val context = system.dispatcher
     lazy val redirectingClient: HttpClient =
       req ⇒ client(req).flatMap(redirectOrResult(redirectingClient)) // recurse to support multiple redirects
 
@@ -90,7 +85,9 @@ object RichHttpClient {
     * @param responseFuture
     * @return
     */
-  def httpResponseToString(responseFuture: Future[HttpResponse])(implicit ec: ExecutionContext, materializer: Materializer): Future[String] = {
+  def httpResponseToString(responseFuture: Future[HttpResponse])(implicit system: ActorSystem = ActorSystem("httpAkka")): Future[String] = {
+    implicit val materializer = ActorMaterializer()
+    implicit val context = system.dispatcher
     responseFuture.flatMap {
       case HttpResponse(StatusCodes.OK, headers, entity, _) =>
         // The below is a Future[String] which is filled when the stream is read. That future is what we return!
@@ -102,5 +99,17 @@ object RichHttpClient {
         resp.discardEntityBytes()
         Future.failed(new Exception(message))
     }
+  }
+
+  def dumpToFile(uri: String, destinationPath:String)(implicit system: ActorSystem = ActorSystem("httpAkka")) = {
+    val redirectingClient = getClientWithAkkaSystem()
+    val httpResponseFuture = redirectingClient(HttpRequest(uri = uri))
+    assert(new java.io.File(destinationPath).getParentFile.mkdirs())
+    implicit val ec: ExecutionContext = system.dispatcher
+    implicit val materializer = ActorMaterializer()
+    val fileSink = FileIO.toPath(destinationPath)
+    val ioResultFuture = httpResponseFuture.flatMap(response => {
+      response.entity.dataBytes.runWith(fileSink)
+    })
   }
 }
